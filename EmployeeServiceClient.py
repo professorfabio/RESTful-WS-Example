9e409c6c10b642a652f747273a78f3d938b238df
professorfabio <fmc@ufg.br>
2023-11-15 22:26:52 -0300

    minor

--- EmployeeServiceClient.py
+++ EmployeeServiceClient.py
@@ -25,21 +25,11 @@ def serviceTester():
     response = requests.put(api_url, json=update)
     print (response.json())
     
-    api_url = api_base_url + '/201'+'/40000'
+    # Test update_salary endpoint
+    api_url = api_base_url + '/201'+'/4000'
     print ('Calling PUT on endpoint: ' + api_url)
     response = requests.put(api_url, json=update)
     print (response.json())
-    
-    api_url = api_base_url + '/201/40000/Programmer'
-    print ('Calling PUT on endpoint: ' + api_url)
-    try:
-        response = requests.put(api_url, json=update)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh: 
-        print("HTTP Error") 
-        print(errh.args[0])
-    else:
-        print (response.json())
 
     # Test create_employee endpoint
     api_url = api_base_url
@@ -53,6 +43,18 @@ def serviceTester():
     print ('Calling DELETE on endpoint: ' + api_url)
     response = requests.delete(api_url)
     print (response.json())
+
+    # This endpoint does not exist in the service -- will result in an HTTP 404 error
+    api_url = api_base_url + '/201/40000/Programmer'
+    print ('Calling PUT on endpoint: ' + api_url)
+    try:
+        response = requests.put(api_url, json=update)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh: 
+        print("HTTP Error") 
+        print(errh.args[0])
+    else:
+        print (response.json())
 
 if __name__ == '__main__':
     serviceTester()
